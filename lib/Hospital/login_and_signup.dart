22c490bab29688c6def681076f9cdf7be6e4ca69
Ebisa Bette <ebisabette@gmail.com>
2025-05-29 01:13:30 +0300

    Update login_and_signup.dart

--- lib/Hospital/login_and_signup.dart
+++ lib/Hospital/login_and_signup.dart
@@ -11,7 +11,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Welcome Back")),
+      appBar: AppBar(title: Text("Welcome Back!")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
